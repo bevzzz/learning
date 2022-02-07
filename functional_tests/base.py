bb4ea0a8b010189fde74cce7fa7288c9e46f8465
bevzzz <dmytro.y.solovei@gmail.com>
2022-02-07 09:25:52 +0100

    Added a generic wait method and wrote a test for item validation

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -34,4 +34,13 @@ class FunctionalTest(StaticLiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-
+    @staticmethod
+    def wait_for(fn):
+        start_time = time.time()
+        while True:
+            try:
+                return fn()
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
