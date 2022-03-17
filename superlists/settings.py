3b8a6893652c882b97e0d7676326d8348950fe42
AzureAD\DmytroSolovei <ds@ds-s.at>
2022-03-17 20:31:54 +0100

    use correct email setting var

--- superlists/settings.py
+++ superlists/settings.py
@@ -137,3 +137,22 @@ EMAIL_HOST_USER = config("EMAIL_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_PASSWORD")
 EMAIL_PORT = config("EMAIL_PORT")
 EMAIL_USE_TLS = True
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
+
